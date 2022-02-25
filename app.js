8b3486976bef6efe4a6b7d56627cfc6e551df21d
SabujMahonto <64306095+SabujMahonto@users.noreply.github.com>
2022-02-25 20:40:58 +0600

    task add to localStorage

--- app.js
+++ app.js
@@ -57,19 +57,38 @@ taskList.addEventListener('click',function(event){
 })
 
 // function for delete item 
-
 function deleteItem(event){
     event.target.parentElement.remove()
+    const taskName = event.target.parentElement.firstElementChild.innerText;
+    deleteTaskFromLocalStorage()
 }
 
-// function for completed task  
+// delete task from local storage 
+function deleteTaskFromLocalStorage(taskName){
+    const tasks = getTaskFromLocalStorage()
+    const index = tasks.indexOf(taskName);
+    tasks.splice(index, 1)
+    setTaskToLocalStorage(tasks);
+    
+}
 
+
+// set task to localStorage 
+function setTaskToLocalStorage(tasks){
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+
+}
+
+
+
+
+
+// function for completed task  
 function CompleteTask(event){
    const li = event.target.parentElement.firstElementChild;
   li.classList.toggle('complied_task')
 }
 //  function for edit task name 
-
 function editTaskName(event){
    const li = event.target.parentElement.firstElementChild 
     const previousText = li.innerHTML;
@@ -90,5 +109,42 @@ function editTaskName(event){
     })
     li.appendChild(input)
     event.target.style.display = 'none'
+}
+
+// when load the task 
+document.body.onload = function(e){
+   const taskList = getTaskFromLocalStorage()
 
 }
+
+// get task from localStorage 
+function getTaskFromLocalStorage(){
+    let task;
+    const data = JSON.parse(localStorage.getItem("task"))
+   if(data){
+       task = JSON.parse(data);
+   }else{
+       task = [];
+   }
+   return task
+}
+
+// rendering the task on the UI 
+function displayTaskOnUi(tasks){
+
+        tasks.forEach(task => {
+            const item = document.createElement('div');
+            item.className = item;
+            item.innerHTML = `<div class="item">
+                                <li>${task}</li>
+                                <button class="edit"><i class="fas fa-pen"></i></button>
+                                <button class="complied"><i class="fas fa-check"></i></button>
+                                <button class="delete"><i class="fas fa-trash-can"></i></button>
+                            </div>
+            
+            `;
+            taskList.appendChild(item)
+        })
+
+}
+
